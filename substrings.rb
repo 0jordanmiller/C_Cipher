10dc8d1287e062b858586b189840d7c8bde08adb
Jordan Miller <0jordanmiller@gmail.com>
2020-04-30 17:30:24 -0400

    it works for one word input

--- substrings.rb
+++ substrings.rb
@@ -1,31 +1,27 @@
 # frozen_string_literal: true
-
-# dictionary = %w[down go going horn how howdy it i low own below part partner sit brown]
-dictionary = %w[ow low below]
-# dictionary = %w[below low]
-
-
-
+dictionary = %w[low down ow go going horn how howdy it i in own below part partner sit brown]
 
 def substrings(input, dict)
   hash = {}
-  split = input.split('')
-  dict_letter_count = 0
+  split = input.downcase.split('')
   
+# Outer loop for each word in dictionary array
+
+
 dict.each_with_index do |dict_word, dict_i|
+# set dictionary letter count to 0 on each loop through
+  dict_letter_count = 0
 
-
-
-  substring = split.map do |letter|
-      puts "letter input: #{letter} letter dict: #{dict[dict_i][dict_letter_count]}  "
-    if letter != dict[dict_i][dict_letter_count]
+# Inner loop comparing the split input array to each letter in the dictionary word. 
+# If input letter is not equal to dictionary letter, then keep comparing input letter to the first letter of the dictionary word
+substring = split.map.with_index do |letter, i|
+    if letter != dict_word[dict_letter_count]
       dict_letter_count = 0
       nil
-    elsif letter == dict[dict_i][dict_letter_count]
+    elsif letter == dict_word[dict_letter_count]
       dict_letter_count += 1
       letter
     end
-
   end
   substring_joined = substring.join('')
 
@@ -34,10 +30,22 @@ if substring_joined == dict[dict_i]
   hash[substring_joined] = 1
 end
 end
+puts hash
 
 end
 
 substrings('below', dictionary)
+
+
+
+
+
+
+
+
+
+
+
 
 #
 # The program starts with the first letter of the input. It checks against the first letter of the first word in dictionary until end of the string
