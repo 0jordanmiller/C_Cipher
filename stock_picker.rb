342a11d5b5fe2e0728d714d6a6a6c5becea69aa0
Jordan Miller <0jordanmiller@gmail.com>
2020-05-07 23:43:33 -0400

    stock_picker finished

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,26 +1,32 @@
 # frozen_string_literal: true
 
 def stock_picker(stocks)
-  stocks = stocks.reverse
-  p stocks
   max_sell = 0
   array = []
 
-  stocks.each.with_index do |stock_sell, s|
-    stocks.each.with_index do |stock_buy, b|
-      if stock_sell - stock_buy > max_sell
-        max_sell = stock_sell - stock_buy
-        array = [b, s]
+  stocks.each.with_index do |stock_buy, i|
+    j = i
+    while j < stocks.length
+      sell_price = stock_buy - stocks[j]
+      if sell_price < max_sell
+        max_sell = sell_price
+        array = [i, j]
       end
+      j += 1
     end
   end
-  print array
-  print max_sell
 end
 
-stock_list = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+stock_list = [17, 3, 6, 9, 8, 15, 6, 1, 10]
 
 stock_picker(stock_list)
+
+# stocks.each.with_index do |stock_buy, b|
+#   if stock_sell - stock_buy > max_sell
+#     max_sell = stock_sell - stock_buy
+#     array = [b, s]
+#   end
+# end
 
 # PSEUDOCODE
 
