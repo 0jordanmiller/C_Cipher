5ee6b9ffbfd7e781afe4786858283514ce750c25
Jordan Miller <0jordanmiller@gmail.com>
2020-05-24 22:37:13 -0400

    testing

--- ruby_projects_1/stock_picker.rb
+++ ruby_projects_1/stock_picker.rb
@@ -20,19 +20,3 @@ end
 stock_list = [17, 3, 6, 9, 8, 15, 6, 1, 10]
 
 stock_picker(stock_list)
-
-# stocks.each.with_index do |stock_buy, b|
-#   if stock_sell - stock_buy > max_sell
-#     max_sell = stock_sell - stock_buy
-#     array = [b, s]
-#   end
-# end
-
-# PSEUDOCODE
-
-# variable 'max_sell' to hold the highest difference
-# variable holding the returned array
-
-# Check if element value is lower than 'max_sell'
-# From the last element of the array, subtract each preceding element by that number. If difference is larger, replace 'max_sell'
-# push element's index to the array
