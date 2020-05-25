979f99aee8e2ab9c308b0e34ebda2a026738b6e6
Jordan Miller <0jordanmiller@gmail.com>
2020-05-24 22:25:35 -0400

    a

--- substrings.rb
+++ substrings.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
-
-dictionary = %w[below be down go going horn how howdy it i low own part partner sit]
+dictionary = ["below", "be", "down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(input, dict)
   hash = {}
   split = input.downcase.split(' ')
-
+  
   # 1st loop to split inputted strings and iterate them
   split.each do |input_w|
     # 2nd loop to iterate by the dictionary
     dict.each_with_index do |dict_word, dict_i|
       dict_letter_count = 0
-
-      # 3rd loop comparing the split input array to each letter in the dictionary word.
-      # If input letter is not equal to dictionary letter, then keep comparing input letter to the first letter of the dictionary word
-      substring = input_w.chars.map do |letter|
+    
+    # 3rd loop comparing the split input array to each letter in the dictionary word. 
+    # If input letter is not equal to dictionary letter, then keep comparing input letter to the first letter of the dictionary word
+    substring = input_w.chars.map do |letter|
         if letter != dict_word[dict_letter_count]
           dict_letter_count = 0
           nil
@@ -25,17 +24,29 @@ def substrings(input, dict)
       end
       substring_joined = substring.join('')
 
-      if (substring_joined == dict[dict_i]) && hash.key?(substring_joined)
+      if substring_joined == dict[dict_i] and hash.has_key? substring_joined
         hash[substring_joined] += 1
       elsif substring_joined == dict[dict_i]
         hash[substring_joined] = 1
       end
     end
   end
-  puts hash
+puts hash
+
 end
 
-substrings('below', dictionary)
+substrings('Below Below below low how howdy', dictionary)
+
+
+
+
+
+
+
+
+
+
+
 
 #
 # The program starts with the first letter of the input. It checks against the first letter of the first word in dictionary until end of the string
